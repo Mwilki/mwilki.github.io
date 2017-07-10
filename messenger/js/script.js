21dbb5ed76a8a38e854cf1142edd2ee2f1c77fff
Matthew <mattwilk90@gmail.com>
2017-07-10 18:49:07 -0500

    Add files via upload

--- messenger/js/script.js
+++ messenger/js/script.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
     var room;
     var company = "[ENTER HOTEL]";
     var city = "[ENTER CITY]";
-    var startTicks;
+    // var startTicks;
     var time;
     var message;
     var firstName;
     var lastName;
 
-    //  invoking methods
+        // invoke template function to grab appropriate time-of-day message
     loadTemplate();
     loadGuests();
     loadCompanies();
@@ -29,69 +29,77 @@ $(document).ready(function(){
     });
 });
 
-    function loadTemplate(){
-        // choose appropriate template data and change mock data accordingly.
-        var date = new Date();
-        var hours = date.getHours();
-        $.getJSON('Template.json', function(data){
-            if (hours >= 15 && hours <= 24){
-                time = data.greeting_start.evening;
-            }
-            else if (hours >= 0 && hours <= 11){
-                time = data.greeting_start.morning;
-            }
-            else {
-                time = data.greeting_start.afternoon;
-            }
+function loadTemplate(){
+    // choose appropriate template data.
+    var date = new Date();
+    var hours = date.getHours();
+    $.getJSON('Template.json', function(data){
+        if (hours >= 15 && hours <= 24){
+            time = data.greeting_start.evening;
+        }
+        else if (hours >= 0 && hours <= 11){
+            time = data.greeting_start.morning;
+        }
+        else {
+            time = data.greeting_start.afternoon;
+        }
+    });
+}
+
+// updates guest name and associated data
+function changeGuest(index, company, city){
+    $.getJSON('Guests.json', function(data){
+        guest = data[index].firstName;
+        room = data[index].reservation.roomNumber;
+        // startTicks = data[index].reservation.startTimestamp;
+        firstName = data[index].firstName;
+        lastName = data[index].lastName;
+        // thats a lot of arguments
+        updateGuestInfo(guest, room, company, city, firstName, lastName);
+    });
+}
+
+function updateGuestInfo(guest, room, company, city, firstName, lastName){
+        $("#guestInfo").empty();
+        $("#guestInfo").append("<p id='fullname'> Guest - " + firstName + " " + lastName + "</p>");
+        message = time + " " + guest + ", and welcome to " + company + ". Room #" + room + " is now available for you" + ". Feel free to reach out to us anytime, our concierge desk is available 24/7 for ticket prices, tour locations, and the best meals in " + city + "!";
+        $("#email").val(firstName + "." + lastName + "@aol.com");
+        $("#description").val(message);
+}
+
+// updates company name and associated data
+function changeCompany(index){
+    $.getJSON('Companies.json', function(data){
+        company = data[index].company;
+        city = data[index].city;
+        // timeZone = data[index].timezone;
+        updateCompanyInfo(company, city);
+    });
+}
+
+function updateCompanyInfo(company, city){
+        message = time + " " + guest + ", and welcome to " + company + ". Room #" + room + " is now available for you" + ". Feel free to reach out to us anytime, our concierge desk is available 24/7 for ticket prices, tour locations, and the best meals in " + city + "!";
+        $("#email").val(firstName + "." + lastName + "@aol.com");
+        $("#description").val(message);
+}
+
+// populate dropdown for Guest names
+function loadGuests(){
+    $.getJSON('Guests.json', function(data) {
+        $.each( data, function( key, value ) {
+            $("#guests").append('<option value="' + value.id + '">' + value.firstName + '</option>');
         });
+    },JSON);
+}
 
-    // updates guest name and associated data
-    }
-    function changeGuest(index, company, city){
-        $.getJSON('Guests.json', function(data){
-            guest = data[index].firstName;
-            room = data[index].reservation.roomNumber;
-            startTicks = data[index].reservation.startTimestamp;
-            firstName = data[index].firstName;
-            lastName = data[index].lastName;
-            $("#guestInfo").empty();
-            $("#guestInfo").append("<p id='fullname'> Guest - " + firstName + " " + lastName + "</p>");
-            message = time + " " + guest + ", and welcome to " + company + ". Room #" + room + " is now available for you" + ". Feel free to reach out to us anytime, our concierge desk is available 24/7 for ticket prices, tour locations, and the best meals in " + city + "!";
-            $("#email").val(firstName + "." + lastName + "@aol.com");
-            $("#description").val(message);
+// populate dropdown for Company names
+ function loadCompanies(){
+    $.getJSON('Companies.json', function(data) {
+        $.each( data, function( key, value ) {
+            $("#companies").append('<option value="' + value.id + '">' + value.company + '</option>');
         });
-    }
-
-    // updates company name and associated data
-    function changeCompany(index){
-        $.getJSON('Companies.json', function(data){
-            company = data[index].company;
-            city = data[index].city;
-            timeZone = data[index].timezone;
-            message = time + " " + guest + ", and welcome to " + company + ". Room #" + room + " is now available for you" + ". Feel free to reach out to us anytime, our concierge desk is available 24/7 for ticket prices, tour locations, and the best meals in " + city + "!";
-            $("#email").val(firstName + "." + lastName + "@aol.com");
-            $("#description").val(message);
-        });
-    }
-    
-    // populate dropdown for Guest names
-    function loadGuests(){
-        $.getJSON('Guests.json', function(data) {
-            $.each( data, function( key, value ) {
-                $("#guests").append('<option value="' + value.id + '">' + value.firstName + '</option>');
-            });
-        },JSON);
-    }
-
-
-    // populate dropdown for Company names
-    function loadCompanies(){
-        $.getJSON('Companies.json', function(data) {
-            $.each( data, function( key, value ) {
-                $("#companies").append('<option value="' + value.id + '">' + value.company + '</option>');
-            });
-        },JSON);
-    }
+    },JSON);
+}
 
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -105,30 +113,42 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, determine if message will be sent.
 btn.onclick = function(event) {
     event.preventDefault();
+    $("#success").text(validateFields());
+    modal.style.display = "block";
+}
+
+// return message
+function validateFields(){
+    var message = "";
     if ($("#description").val() == "")
     {
-        $("#success").text("Cannot send blank message...");
+        message = "Cannot send a blank message...";
     } 
     else if ($("#description").val().includes("[ENTER HOTEL]")) 
     {
-        $("#success").text("Please enter hotel name...");
+        message = "Please enter hotel name...";
     }
     else if($("#email").val() == ""){
-        $("#success").text("Please enter an email...");
+        message = "Please enter an email...";
     }
     else {
-        $("#success").text("sending...");
-        // so much real
+        message = "sending...";
+        // so authentic
         setTimeout(function(){
-            $("#email").val("");
-            $("#companies").val(0);
-            $("#guests").val(0);
-            $("#description").val("");
-            $("#fullname").text("sent message!");
+            reset();
             modal.style.display = "none";
         }, 2000);
     }
-    modal.style.display = "block";
+    return message;
+}
+
+// reset all the fields
+function reset(){
+    $("#email").val("");
+    $("#companies").val(0);
+    $("#guests").val(0);
+    $("#description").val("");
+    $("#fullname").text("sent message!");
 }
 
 // When the user clicks on <span> (x), close the modal
